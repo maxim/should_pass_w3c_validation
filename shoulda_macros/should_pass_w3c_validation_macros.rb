24cca57e67bb40a3d6ad174a944bbae0fc9e369d
Maxim Chernyak <max@bitsonnet.com>
2009-03-16 15:51:35 -0400

    Initial commit.

---
+++ shoulda_macros/should_pass_w3c_validation_macros.rb
@@ -0,0 +1,31 @@
+require 'w3c_validators'
+
+module ShouldPassW3cValidationMacros
+  include W3CValidators
+  
+  def should_pass_w3c_validation(*urls)
+    should "pass w3c validation" do
+      tempfile = Tempfile.new('test')
+      tempfile.write(@response.body)
+      tempfile.flush
+      
+      sleep 0.5 # Take a nap
+      
+      validator = MarkupValidator.new
+      results = validator.validate_file(tempfile.path)
+      valid = [results.errors, results.warnings].all?{ |set| set.size == 0 }
+      fail_text = ''
+      unless valid
+        fail_text = (results.errors + results.warnings).inject('') do |text, message| 
+          message = message.to_s.sub(/URI:[^;]+;/, '')
+          text << "\n#{message}"
+        end
+      end
+      assert valid, fail_text
+    end  
+  end
+end
+
+class ActionController::TestCase
+  extend ShouldPassW3cValidationMacros
+end
